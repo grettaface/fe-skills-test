bba38ae52195555029d33ced7bd7935adeab66c0
Bret Kruse <bret.kruse@gmail.com>
2018-03-13 20:42:39 -0400

    Updated tests

--- src/components/pages/IndexPage.spec.js
+++ src/components/pages/IndexPage.spec.js
@@ -1,31 +1,74 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { expect } from 'chai';
+import sinon from 'sinon';
+
 // eslint-disable-next-line no-unused-vars
 import JSDOM from '../../../test/helpers/jsdomHelper';
+import contxtSDK from '../../services/ContxtService';
+
+import facilities from '../../../test/fixtures/facilities.json';
 
 import IndexPage from './IndexPage';
 
 describe('IndexPage', function() {
   let wrapper;
+  let sandbox;
+  const sampleOrg = {
+    id: '59270c25-4de9-4b22-8e0b-ab287ac344ce',
+    name: 'Novozymes',
+    created_at: '2018-03-07T00:09:53.019Z',
+    updated_at: '2018-03-07T00:09:53.019Z'
+  };
+
+  before(function() {
+    sandbox = sinon.sandbox.create();
+  });
 
   beforeEach(function() {
-    wrapper = shallow(<IndexPage />);
+    sandbox.stub(contxtSDK.facilities, 'getAll').resolves(facilities);
+    wrapper = mount(<IndexPage />);
+  });
+
+  afterEach(function() {
+    sandbox.restore();
   });
 
   it('should render', function() {
     expect(wrapper).to.not.be.empty;
   });
 
-  it('should properly show a facilities list', function() {
-
+  describe('constructor', function() {
+    it('should properly set my starting state', function() {
+      const {facilities, organizations, filteredFacilities} = wrapper.state();
+      expect(facilities).to.not.be.undefined;
+      expect(organizations).to.not.be.undefined;
+      expect(filteredFacilities).to.not.be.undefined;
+    });
   });
 
-  it('should properly show an organization dropdown', function() {
-
+  describe('componentDidMount', function() {
+    it('should properly set the state for facilities and organizations', function() {
+      wrapper.update();
+      const {facilities, organizations} = wrapper.state();
+      expect(facilities.length).to.equal(facilities.length);
+      expect(organizations.length).to.be.above(0);
+    });
   });
 
-  it('should properly update the facilities list when choosing a new organization', function() {
+  describe('organization selected', function() {
+    it('should properly set the state for facilities and organizations', function() {
+      const facilityCache = wrapper.state().filteredFacilities;
+      wrapper.instance().organizationSelected(sampleOrg);
+      wrapper.update();
+      expect(wrapper.state().filteredFacilities).to.not.equal(facilityCache);
+    });
+  });
 
+  describe('facility filtering', function() {
+    it('should properly filter the facilities based on an org', function() {
+      expect(wrapper.instance().filterFacilities(facilities, sampleOrg).length)
+        .to.be.below(facilities.length);
+    });
   });
 });
